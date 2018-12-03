5bf4fac52000bd9cadbecd5412bc8f351e5cd8d6
ranjeet <ranjeet.leecare@gmail.com>
2018-12-03 16:24:40 +1100

    refactored code.

--- allocateRequests.js
+++ allocateRequests.js
@@ -1,8 +1,10 @@
 var mongo = require('mongodb');
 var url = 'mongodb://localhost:27017/mydb';
+var fs = require('fs');
 
 var returnValue = '{"butlers": [{"requests": []}],"spreadClientIds": []}';
 var returnJSON = JSON.parse(returnValue);
+var setOfClientIds = new Set();
 
 mongo.connect(url, { useNewUrlParser: true }, function (err, db) {
 	console.log(returnJSON);
@@ -12,25 +14,18 @@ mongo.connect(url, { useNewUrlParser: true }, function (err, db) {
 		var hours = 0;
 		var butlerId = 0;
 		for (request in result) {
+			setOfClientIds.add(result[request].clientId);
 			var newRequestHours = result[request].hours;
 			if ((hours + newRequestHours) > 8) {
-				butlerId = butlerId + 1;
-				console.log('create butler ' + newRequestHours + ' ' + result[request].requestId);
-				var butlerData = { "_id": butlerId, "requests": [result[request].requestId] };
-				returnJSON.butlers.push(butlerData);
+				var butlerData;
+				({ butlerData, butlerId } = createNewButler(butlerId, newRequestHours, result, butlerData));
 				hours = 0;
 			} else {
 				if (hours == 0) {
-					butlerId = butlerId + 1;
-					console.log('create butler ' + newRequestHours + ' ' + result[request].requestId);
-					var butlerData = { "_id": butlerId, "requests": [result[request].requestId] };
-					returnJSON.butlers.push(butlerData);
+					var butlerData;
+					({ butlerData, butlerId } = createNewButler(butlerId, newRequestHours, result, butlerData));
 				} else {
-					console.log('adding to existing butler ' + newRequestHours);
-					var index = returnJSON.butlers.length - 1;
-					console.log(returnJSON.butlers[index]);
-					var butlerRequests = returnJSON.butlers[index].requests;
-					butlerRequests.push(result[request].requestId)
+					addTaskToExistingButler(newRequestHours, result);
 				}
 			}
 			hours = hours + newRequestHours;
@@ -38,7 +33,30 @@ mongo.connect(url, { useNewUrlParser: true }, function (err, db) {
 				hours = 0;
 		}
 		console.log('======');
+		for (let clientId of setOfClientIds) {
+			returnJSON.spreadClientIds.push(clientId);
+		}
 		console.log(returnJSON);
-	});
+		fs.writeFile('./output.json', JSON.stringify(returnJSON), function (err) {
+			if (err) throw err;
+			console.log('Output Saved!');
+		  });
+		});
 	db.close();
 });
+
+function addTaskToExistingButler(newRequestHours, result) {
+	console.log('adding to existing butler ' + newRequestHours);
+	var index = returnJSON.butlers.length - 1;
+	console.log(returnJSON.butlers[index]);
+	var butlerRequests = returnJSON.butlers[index].requests;
+	butlerRequests.push(result[request].requestId);
+}
+
+function createNewButler(butlerId, newRequestHours, result, butlerData) {
+	butlerId = butlerId + 1;
+	console.log('create butler ' + newRequestHours + ' ' + result[request].requestId);
+	var butlerData = { "requests": [result[request].requestId] };
+	returnJSON.butlers.push(butlerData);
+	return { butlerData, butlerId };
+}
